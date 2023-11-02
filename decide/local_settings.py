73604190fa5d5d1b7b340b55ec11da4d13293cb6
Francisco Javier Barba Trejo <101475909+barbaat@users.noreply.github.com>
2023-11-02 15:53:08 +0000

    Añadido local_settings.py para práctica de Docker

---
+++ decide/local_settings.py
@@ -0,0 +1,42 @@
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+base_url = 'http://localhost:8000'
+
+APIS = {
+    'authentication': base_url,
+    'base': base_url,
+    'booth': base_url,
+    'census': base_url,
+    'mixnet': base_url,
+    'postproc': base_url,
+    'store': base_url,
+    'visualizer': base_url,
+    'voting': base_url,
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decidedb',
+        'USER': 'decideuser',
+        'PASSWORD': 'decidepass123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
