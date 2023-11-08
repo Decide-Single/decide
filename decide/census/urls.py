9eb7c380feeb6ae1a7157d810092b0e53b37c7c9
tcabgom <tcabgom222@gmail.com>
2023-11-08 20:29:08 +0100

    Tarea-301. Crea función de importación

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from . import views
-
+from .views import CensusImportView
 
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
+    path('import/', CensusImportView.as_view(), name='import_census'),
 ]
--- decide/census/views.py
+++ decide/census/views.py
@@ -1,7 +1,11 @@
+import csv
 from django.db.utils import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
+from django.views import View
+from django.http import JsonResponse
 from rest_framework.status import (
         HTTP_201_CREATED as ST_201,
         HTTP_204_NO_CONTENT as ST_204,
@@ -49,3 +53,28 @@ class CensusDetail(generics.RetrieveDestroyAPIView):
         except ObjectDoesNotExist:
             return Response('Invalid voter', status=ST_401)
         return Response('Valid voter')
+
+class CensusImportView(View):
+
+    template_name = 'import_census.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            file = request.FILES.get('file')
+            if file and file.name.endswith('.csv'):
+                try:
+                    decoded_file = file.read().decode('utf-8').splitlines()
+                    reader = csv.reader(decoded_file)
+                    for index, row in enumerate(reader):
+                        voting_id, voter_id = row  # Assuming the CSV structure is: voting_id, voter_id
+                        Census.objects.create(voting_id=voting_id, voter_id=voter_id)
+                    return JsonResponse({'message': 'Census imported successfully'}, status=201)
+                except Exception as e:
+                    return JsonResponse({'error': 'Error trying to create census: {}'.format(str(e))}, status=409)
+            else:
+                return JsonResponse({'error': 'Invalid or no file provided'}, status=400)
+        else:
+            return JsonResponse({'error': 'Invalid request method'}, status=405)
