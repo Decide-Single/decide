01539834ccb04f50d21a3c82d522122310db65b9
Francisco Javier Barba Trejo <101475909+barbaat@users.noreply.github.com>
2023-11-15 18:45:16 +0000

    Add sex and locality fields to Census model

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
-
 class Census(models.Model):
+    SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True, null=True)
+    locality = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
