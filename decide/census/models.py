394e5b8870d57ffceff302a7f4baf23056d8cbe9
Ignarrman <nacho.arromant@gmail.com>
2023-11-17 19:07:54 +0100

    conf: Configura plantilla de commits #36

--- decide/census/models.py
+++ decide/census/models.py
@@ -21,6 +21,7 @@ class Census(models.Model):
     has_voted = models.BooleanField(default=False)
     vote_result = models.CharField(max_length=255, blank=True, null=True)
     vote_method = models.CharField(max_length=10, choices=VOTE_METHOD_CHOICES, blank=True, null=True)
+    #test de un cambio para comprobar que va el template
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
