c9c0efa2e6b84581b368ccf35f609e2f919f6a06
Francisco Javier Barba Trejo <101475909+barbaat@users.noreply.github.com>
2023-12-11 15:12:47 +0000

    fix: Añadidas variables de entorno en github y cambiado algunas propiedades de atributos (II)

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
