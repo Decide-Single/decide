92260e6e83b3e79c5fe2037d26d1660f864caa4d
Francisco Javier Barba Trejo <101475909+barbaat@users.noreply.github.com>
2023-12-12 08:52:01 +0000

    fix: Corregir problema con despliegue de render, error 500

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ["*"]
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = [".joaquin1.onrender.com","localhost"]
 
 # Application definition
 
